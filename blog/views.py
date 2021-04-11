5165c7dd613dbe205bce0124eaab9fbb6783fbcb
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 00:53:08 -0700

    added filter

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from .filters import BlogFilter
 
 
 def blog(request):
-    return render(request, 'portal/blog-home.html')
+    blogs = Blog.objects.filter(published=True).order_by('-created')
+    filters = BlogFilter(request.GET, queryset=blogs)
+    blogs = filters.qs
+
+    context = {
+        'blogs': blogs,
+        'filters':filters
+    }
+    return render(request, 'portal/blog-home.html', context)
