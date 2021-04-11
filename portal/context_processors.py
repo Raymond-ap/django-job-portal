09e299fa05fdae4d0109cec7fd27f806ecb11568
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 06:56:46 -0700

    commit

--- portal/context_processors.py
+++ portal/context_processors.py
@@ -6,10 +6,12 @@ from blog.models import *
 def Processors(request):
     jobs = Job.objects.filter(approved=True).order_by('-created')
     categories = Category.objects.all().order_by('-id')
-    blog_count = Blog.objects.filter(published=True).count()
+    blogs = Blog.objects.filter(published=True).order_by('-id')
+    blog_count = blogs.count()
 
     return {
         'categories': categories,
         'jobs': jobs,
-        'blog_count':blog_count
+        'blog_count': blog_count,
+        'blogs': blogs,
     }
