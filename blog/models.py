6a93d45a41fe53e348b6662c95ec5f827fcb2c42
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 00:17:08 -0700

    blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(
+        upload_to="blog/images", blank=True, null=True)
+    category = models.CharField(max_length=150)
+    description = RichTextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
