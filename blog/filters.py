7fff164347f632b3d9da30c9d6aacf927eced8e3
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 00:52:27 -0700

    added blog filters

---
+++ blog/filters.py
@@ -0,0 +1,19 @@
+import django_filters
+from django import forms
+from .models import *
+
+
+class BlogFilter(django_filters.FilterSet):
+    class Meta:
+        model = Blog
+        fields = ('title',)
+
+        filter_overrides = {
+            models.CharField: {
+                'filter_class': django_filters.CharFilter,
+                'extra': lambda f: {
+                    'lookup_expr': 'icontains',
+                    'widget': forms.TextInput(attrs={'class': '', 'placeholder': 'Search blog'})
+            }
+        }
+        }
