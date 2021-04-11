450eed489d4e31421f05e7b67d017acf495c1623
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 00:17:26 -0700

    register blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'published', 'created')
+    search_fields = ('title', 'category')
