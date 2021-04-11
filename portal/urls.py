d71ec7393d05303fb9bafe3d0746632a1ef3610d
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 00:05:03 -0700

    changed blog url

--- portal/urls.py
+++ portal/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', homePage, name="home"),
     path('about', about, name="about"),
     path('jobs', jobs, name="jobs"),
-    path('blog', blog, name="blog"),
     path('contact', contact, name="contact"),
     path('login', loginPage, name="login"),
     path('signup', signup, name="signup"),
