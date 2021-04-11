03656dfc5784592e27125f17056bec74854c0186
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-11 06:09:00 -0700

    added blog count

--- portal/context_processors.py
+++ portal/context_processors.py
@@ -1,12 +1,15 @@
-from .models import *
+from portal.models import *
 from django.db.models import Count
+from blog.models import *
 
 
 def Processors(request):
     jobs = Job.objects.filter(approved=True).order_by('-created')
     categories = Category.objects.all().order_by('-id')
+    blog_count = Blog.objects.filter(published=True).count()
 
     return {
         'categories': categories,
-        'jobs':jobs
+        'jobs': jobs,
+        'blog_count':blog_count
     }
