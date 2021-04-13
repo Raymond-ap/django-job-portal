8a24cfabe0cc7925e99f27247ebb380bb8c57179
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-13 01:00:05 -0700

    inline forms

--- portal/forms.py
+++ portal/forms.py
@@ -1,2 +1,21 @@
 from django import forms
 from .models import *
+from django.forms import ModelForm, inlineformset_factory
+
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = '__all__'
+        exclude = ('slug', 'created', 'featured', 'approved')
+
+
+class JobRequirementForm(forms.ModelForm):
+    class Meta:
+        model = JobRequirement
+        fields = '__all__'
+        exclude = ()
+
+
+JobRequirementFormset = inlineformset_factory(
+    Job, JobRequirement, form=JobRequirementForm, extra=3)
