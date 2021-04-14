e0b3c3377ffbc30e8f60027c46b9e273b15a5dff
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-14 12:12:51 -0700

    update settings

--- job_finder/settings.py
+++ job_finder/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from botocore.client import Config
+import boto3
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -161,3 +164,7 @@ STATIC_ROOT = Path.joinpath(BASE_DIR, 'assests')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
