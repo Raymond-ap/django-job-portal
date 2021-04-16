111cb56c96c19656c37f4f2d9de8397827b920be
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-16 13:17:54 -0700

    generic view

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from portal.models import Category, Job
+from rest_framework import generics, mixins
+from .serializers import JobSerializer
 
-# Create your views here.
+
+class JobAPIView(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = JobSerializer
+    queryset = Job.objects.all()
+
+    def get(self, request):
+        return list(request)
