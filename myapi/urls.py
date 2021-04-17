bba7b0a912d513e5facd0bc74e6604cf1cc0b515
Apungu Raymond <63607228+Raymond-ap@users.noreply.github.com>
2021-04-17 07:26:37 +0000

    Commit

--- myapi/urls.py
+++ myapi/urls.py
@@ -7,9 +7,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Article API Text",
+        title="Job Finder API",
         default_version='v1',
-        description="API for simple article",
+        description="API for job portal",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
